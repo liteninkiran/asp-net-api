49e3329343aeda400ad25bbbea45797138afa4d7
Kiran Anand <liteninkiran@hotmail.com>
2021-09-22 00:03:15 +0100

    Get, Post, Put, Delete requests

---
+++ QuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using QuotesApi.Models;
+
+namespace QuotesApi.Controllers
+{
+    public class QuotesController : ApiController
+    {
+        static List<Quote> _quotes = new List<Quote>()
+        {
+            new Quote(){ id = 0, author = "Einstein", description = "Imagination is more important than knowledge", title = "Imagination" },
+            new Quote(){ id = 1, author = "Einstein", description = "Imagination is more important than knowledge", title = "Imagination" },
+        };
+
+        public IEnumerable<Quote> Get()
+        {
+            return _quotes;
+        }
+
+        public void Post([FromBody]Quote quote)
+        {
+            _quotes.Add(quote);
+        }
+
+        public void Put(int id, [FromBody] Quote quote)
+        {
+            _quotes[id] = quote;
+        }
+
+        public void Delete(int id)
+        {
+            _quotes.RemoveAt(id);
+        }
+    }
+}
+
---
+++ QuotesApi/Models/Quote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuotesApi.Models
+{
+    public class Quote
+    {
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string author { get; set; }
+
+        public string description { get; set; }
+    }
+}
